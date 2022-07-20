87b20097ccb0d915a701142e7a1f45ca2a3c8896
Deepak Raj <66163072+Dpkraj1806@users.noreply.github.com>
2022-07-20 02:19:01 -0700

    Add files via upload

---
+++ bank/main.js
@@ -0,0 +1,16 @@
+var users=[
+    {username: 'Deepak', password: 'deepak',phonenumber:9629474374,address:'1/36A,Santhan Nadar Street,Keelapavoor',acctype:'Savings',balance:0,transactions:[{date:"18:06:2022",type:"deposit",value:1000},{date:"1:07:2022",type:"deposit",value:1000},{date:"1:08:2022",type:"deposit",value:1000},{date:"1:09:2022",type:"deposit",value:1000},{date:"1:10:2022",type:"withdrawal",value:100}]},
+    {username: 'Navin', password: 'navin'},
+    {username: 'Praveen', password: 'praveen'},
+    {username: 'Sathish', password: 'sathish'},
+    {username: 'Athipan', password: 'athipan'},
+    {username: 'Prakash', password: 'prakash'},
+    {username: 'Surya', password: 'surya'},
+    {username: 'Karthi', password: 'karthi'},
+    {username: 'Tharan', password: 'tharan'},
+    {username: 'Raj', password: 'raj'}
+]
+var username=document.getElementById("newuser")
+var password=document.getElementById("newpassword")
+
+
